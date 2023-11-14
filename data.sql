0ed3bd1ad0244fde6f1bf14fd5e6fffe03eeff6c
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2023-11-14 10:59:45 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,16 +1,15 @@
 CREATE DATABASE students;
-use studentsDB;
 
 CREATE TABLE students(name varchar(255), studentID int);
 SHOW COLUMNS FROM students;
-INSERT INTO students (name, studentID) VALUES ('Jordi Parra Crespo' , '28011999');
+INSERT INTO students (name, studentID) VALUES ('jesus azuaje' , '00000000');
 
 CREATE TABLE marks(subject varchar(255), name varchar(255), mark decimal(3,1));
 SHOW COLUMNS FROM marks;
-INSERT INTO marks(subject, name, mark) VALUES ('PBE', 'CDR', 9.8);
-INSERT INTO marks(subject, name, mark) VALUES ('AST', 'PR1', 4.2);
+INSERT INTO marks(subject, name, mark) VALUES ('PBE', 'CDR', 9.2);
+INSERT INTO marks(subject, name, mark) VALUES ('ONELE', 'PR1', 3.7);
 
 CREATE TABLE timetables(day varchar(255),hour varchar(255), subject varchar(255), room varchar(255));
-INSERT INTO timetables(day, hour, subject, room) VALUES ('TUE', '08:00:00', 'TD', 'A4-002');
+INSERT INTO timetables(day, hour, subject, room) VALUES ('MON', '08:00:00', 'RP', 'A4-002');
 CREATE TABLE tasks(date varchar(255), subject varchar(255), name varchar(255));
-INSERT INTO tasks(date, subject, name) VALUES ('2020-7-18','PBE','CDR2');
+INSERT INTO tasks(date, subject, name) VALUES ('2023-9-18','PBE','P1');
