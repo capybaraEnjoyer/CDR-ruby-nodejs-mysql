b9428bdec9038bd5c71d6ff1242f4ea80f9fcde4
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2023-11-15 08:24:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,25 +9,15 @@ const db = mysql.createConnection({
   database: 'your_database',
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to MySQL database');
-});
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/api/resource') {
     // Perform a MySQL query to fetch data
-    db.query('SELECT * FROM your_table', (err, results) => {
+    db.query('SELECT * FROM table', (err, results) => {
       if (err) {
         console.error('Error querying the database:', err);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
         return;
       }
-
       // Respond with JSON data from the database
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(results));
