0d274b3ef353071ca7e97f7fc53bd40a87accf81
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2023-11-21 20:23:38 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,23 +1,27 @@
 CREATE DATABASE if not exists prueba;
 use prueba;
--- ALTER USER 'azcam' IDENTIFIED WITH mysql_native_password BY 'Reproche_2211';
+-- ALTER USER 'GuardiaAlonso'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Jjooaann03';
 -- flush privileges;
 -- Se puede crear otra columna llamada "ID" en marks para utilizar [ FOREIGN KEY (ID) REFERENCES students(studentID) ] ? -> si
 -- Las tablas timetables y tasks son comunes -> si
-CREATE TABLE if not exists students(name varchar(255), 
-									studentID int PRIMARY KEY);
-INSERT INTO students (name, studentID) VALUES ('jesus azuaje' , '00000000');
-INSERT INTO students (name, studentID) VALUES ('Pablo Castillo' , '0000001');
 
-CREATE TABLE if not exists marks(studentID int, 
+GRANT ALL PRIVILEGES ON *.* TO 'GuardiaAlonso'@'localhost' WITH GRANT OPTION;
+CREATE TABLE if not exists students(name varchar(255), 
+									studentID varchar(255) PRIMARY KEY);
+INSERT INTO students (name, studentID) VALUES ('Jesus Azuaje' , 'D65DE4B0');
+INSERT INTO students (name, studentID) VALUES ('Pablo Castillo' , '0000001');
+INSERT INTO students (name, studentID) VALUES ('Joan Guardia' , 'E674E1B0');
+
+CREATE TABLE if not exists marks(studentID varchar(255), 
 								 subject varchar(255), 
                                  name varchar(255), 
                                  mark decimal(3,1),
                                  FOREIGN KEY (studentID) REFERENCES students(studentID));
-INSERT INTO marks(studentID, subject, name, mark) VALUES (0, 'PBE', 'CDR', 9.2);
-INSERT INTO marks(studentID, subject, name, mark) VALUES (1, 'ONELE', 'PR1', 3.7);
+INSERT INTO marks(studentID, subject, name, mark) VALUES ('D65DE4B0', 'PBE', 'CDR', 9.2);
+INSERT INTO marks(studentID, subject, name, mark) VALUES ('0000001', 'ONELE', 'PR1', 3.7);
+INSERT INTO marks(studentID, subject, name, mark) VALUES ('E674E1B0', 'ONELE', 'PR1', 6.5);
 
-/*
+
 CREATE TABLE if not exists timetables(day varchar(255), 
 									  hour varchar(255), 
                                       subject varchar(255), 
@@ -28,4 +32,3 @@ CREATE TABLE if not exists tasks(date varchar(255),
 								 subject varchar(255), 
                                  name varchar(255));
 INSERT INTO tasks(date, subject, name) VALUES ('2023-9-18','PBE','P1');
-*/
