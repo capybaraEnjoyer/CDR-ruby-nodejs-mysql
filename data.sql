9bedc7ba3f09f1407bd547bfd2be8ddbe07c6360
capybaraEnjoyer <108890297+capybaraEnjoyer@users.noreply.github.com>
2023-11-21 20:27:28 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -13,22 +13,23 @@ INSERT INTO students (name, studentID) VALUES ('Pablo Castillo' , '0000001');
 INSERT INTO students (name, studentID) VALUES ('Joan Guardia' , 'E674E1B0');
 
 CREATE TABLE if not exists marks(studentID varchar(255), 
-								 subject varchar(255), 
-                                 name varchar(255), 
-                                 mark decimal(3,1),
-                                 FOREIGN KEY (studentID) REFERENCES students(studentID));
+			   subject varchar(255), 
+                           name varchar(255), 
+                           mark decimal(3,1),
+                           FOREIGN KEY (studentID) REFERENCES students(studentID));
+
 INSERT INTO marks(studentID, subject, name, mark) VALUES ('D65DE4B0', 'PBE', 'CDR', 9.2);
 INSERT INTO marks(studentID, subject, name, mark) VALUES ('0000001', 'ONELE', 'PR1', 3.7);
 INSERT INTO marks(studentID, subject, name, mark) VALUES ('E674E1B0', 'ONELE', 'PR1', 6.5);
 
 
 CREATE TABLE if not exists timetables(day varchar(255), 
-									  hour varchar(255), 
-                                      subject varchar(255), 
-                                      room varchar(255));
+			   hour varchar(255), 
+                           subject varchar(255), 
+                           room varchar(255));
 INSERT INTO timetables(day, hour, subject, room) VALUES ('MON', '08:00:00', 'RP', 'A4-002');
 
 CREATE TABLE if not exists tasks(date varchar(255), 
-								 subject varchar(255), 
-                                 name varchar(255));
+			   subject varchar(255), 
+                           name varchar(255));
 INSERT INTO tasks(date, subject, name) VALUES ('2023-9-18','PBE','P1');
