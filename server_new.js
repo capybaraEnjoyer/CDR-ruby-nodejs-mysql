166ae5007aed0da4d1468567aa3992b55e692284
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2023-12-13 09:24:28 +0100

    Update server_new.js

--- server_new.js
+++ server_new.js
@@ -4,14 +4,12 @@ const path = require('path');
 const url = require('url');
 const port = 3000;
 
-
-
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'GuardiaAlonso',
   password: 'Jjooaann03',
   database: 'prueba',
-  port: 3000,
+  port: 3306,
 });
 
 db.connect((err) => {
@@ -20,18 +18,18 @@ db.connect((err) => {
     return;
   }
   console.log('Connected to MySQL database');
-});
 
+  
+});
 //Creamos el objeto servidor
 const server = http.createServer((req, res) => {
 
   if (req.method === 'GET') {
     const myURL = new URL(`http://${req.headers.host}${req.url}`);
-    const pathname = myURL.pathname;
-    pathname.replace("/");
+    const pathname = myURL.pathname.replace("/", "");
     console.log(pathname);
 
-    if (pathname === 'students') { //CASO DE IDENTIFICACIoN DE ESTUDIANTE.
+    if (pathname === 'students') { //CASO DE IDENTIFICACION DE ESTUDIANTE.
       console.log("entrar");
       idToFind = myURL.searchParams.get('uid');
       const query = 'SELECT * FROM students WHERE studentID = ?';
@@ -62,16 +60,23 @@ const server = http.createServer((req, res) => {
       var primero = true;
 
       for (const key in constraintsSeparadas) {
-        if(key === 'uid' && pathname != 'marks'){}
+        if (key === 'uid' && pathname != 'marks') {
+          continue;
+        }
+        else if (key === 'uid' && pathname == 'marks') {
+          idToFind = myURL.searchParams.get('uid');
+          query += ` WHERE studentID = ${idToFind}`
+        }
         else if (primero) {
-          query += `WHERE ${key} = ${result[key]}`
+          query += ` WHERE ${key} = ${constraintsSeparadas[key]}`
           primero = false;
         }
         else if (!primero) {
-          query += `AND WHERE ${key} = ${result[key]}`
+          query += ` AND ${key} = ${constraintsSeparadas[key]}`  
         }
       }
-      
+      query += ";";
+      console.log(query);
       db.query(query, (err, results) => {
         if (err) {
           console.error('An error occurred while executing the query', err);
@@ -80,9 +85,9 @@ const server = http.createServer((req, res) => {
           return;
         }
         if (results.length > 0) {
-          console.log(response);
+          console.log("response");
           res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(response));
+          res.end(JSON.stringify(results));
 
         } else {
           console.log('No user found with the specified ID.');
@@ -125,27 +130,5 @@ function manejarConstraints(url) {
 }
 
 server.listen(port, () => {
-  console.log(`Server is listening on ${port}/`);
-  /*
-  const query = 'SELECT mark FROM marks WHERE studentID = 0;';
-
-  db.query(query, (err, results) => {
-    if (err) throw err;
-
-    // Display each row's column value
-    results.forEach(row => {
-      console.log(row.mark);
-    });
-  });
-  */
-})
-
-
-const urlEjemplo = "https://ejemplo.com/ruta?constraint1=valor1&constraint2=valor2&constraint3[gt]=5&constraint4[lte]=10&constraint5[gte]=3"
-
-const result = manejarConstraints(urlEjemplo);
-for (const key in result) {
-  console.log(`Key: ${key}, Value: ${result[key]}`);
-}
-console.log("Constraints separadas:");
-console.log(result);
+    console.log(`Server is listening on ${port}/`);
+}); 
