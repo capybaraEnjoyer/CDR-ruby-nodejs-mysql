2904750ceff3d0c04b0e6043cbc177ab9ad33361
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2023-12-16 18:54:45 +0100

    Update server_new.js

--- server_new.js
+++ server_new.js
@@ -30,26 +30,100 @@ const server = http.createServer((req, res) => {
     console.log(pathname);
     const constraintsSeparadas = manejarConstraints(myURL);
     var query = `SELECT * FROM ${pathname}`;
+    var query1 = null;
     var primero = true;
-
+    var controlDay = false;
+    var controlHour = false;
+    var resultat1;
+    console.log(constraintsSeparadas);
     for (const key in constraintsSeparadas) {
+      if (pathname == 'timetables') {
+        if (key == 'day') {
+          constraintsSeparadas[key] = convertirDiaTextoANumero(constraintsSeparadas[key]);
+          controlDay = true;
+          controlHour = true; 
+        } else if (key == 'hour') {
+          controlHour = true;
+          controlDay = true; 
+        }
+      }
       if (key === 'uid' && (pathname == 'marks' || pathname == 'students')) {
-        idToFind = myURL.searchParams.get('uid');
-        query += ` WHERE ${key} = "${idToFind}"`
-      }
-      else if(key === 'uid'){
+        idToFind = myURL.searchParams.get('uid'); // Utilizo parsedUrl en lugar de myURL
+        if (pathname == 'marks') {
+          if (primero) {
+            query += ` WHERE ${key} = "${idToFind}"`;
+            primero = false;
+          } else {
+            query += ` AND ${key} = "${idToFind}"`;
+          }
+        }
+        else{
+          query += ` WHERE ${key} = "${idToFind}"`;
+        }
+
+      } else if (key === 'uid') {
         continue;
-      }
-      else if (primero) {
-        query += ` WHERE ${key} = "${constraintsSeparadas[key]}"`
-        primero = false;
-      }
-      else if (!primero) {
-        query += ` AND ${key} = "${constraintsSeparadas[key]}"`
+      } else {
+        const keyValue = constraintsSeparadas[key];
+        const keyOperators = constraintsSeparadas[key];
+  
+        if (keyOperators && Object.keys(keyOperators).length > 0) {
+          const ops = Object.keys(keyOperators);
+          for (const op of ops) {
+            if (keyValue[op] !== null && keyValue[op] !== undefined) {
+              if (primero) {
+                query += ` WHERE ${key} ${op} "${keyValue[op]}"`;
+                primero = false;
+              } else {
+                query += ` AND ${key} ${op} "${keyValue[op]}"`;
+              }
+            }
+          }
+        } else {
+          if (primero) {
+            query += ` WHERE ${key} = "${keyValue}"`;
+            primero = false;
+          } else {
+            query += ` AND ${key} = "${keyValue}"`;
+          }
+        }
       }
     }
+    const now = new Date();
+    const day = now.getDay().toString();
+    const hh = now.getHours().toString().padStart(2, 0);
+    const mm = now.getMinutes().toString().padStart(2, 0);
+    const hora = hh + ":" + mm;
+    
+    if (pathname == 'timetables' && controlDay == false) {
+      
+      if (primero) {
+        //query1 += `SELECT * FROM ${pathname}` + query
+        query += ` WHERE day >= "2"`
+        primero = false;
+        //query1 += ` WHERE day < "${day}"`
+      }
+      else {
+        //query1 += `SELECT * FROM ${pathname}` + query
+        query += ` AND day >= "2"`
+        //query1 += ` AND day < "${day}"`
+      }
+    }
+    if(pathname == 'timetables' && controlHour == false){
+      if (primero) {
+        //query1 += `SELECT * FROM ${pathname}` + query
+        query += ` WHERE hour >= "${hora}"`
+        primero = false;
+        //query1 += ` WHERE day < "${day}"`
+      }
+      else {
+        query += ` AND hour >= "${hora}"`
+      }
+    }
+
     query += " ;";
     console.log(query);
+    //var resultat;
     db.query(query, (err, results) => {
       if (err) {
         console.error('An error occurred while executing the query', err);
@@ -57,8 +131,15 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ 'error': 'Internal server error' }));
         return;
       }
+      results.forEach(result => {
+        if (result.hasOwnProperty('day')) {
+          const numeroDia = parseInt(result.day);
+          result.day = convertirNumeroADiaTexto(numeroDia);
+        }
+      });
       if (results.length > 0) {
-        console.log("response");
+        //resultat = results;
+        console.log(results);
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(results));
 
@@ -67,35 +148,62 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ 'message': 'No user found with the specified ID.' }));
       }
     });
-
-
+    /*if (query1 != null) {
+      
+      db.query(query, (err, results1) => {
+        if (results1.length > 0) {
+          resultat1 = results1;
+          console.log(results1);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(results1));
+        }
+      });
+      var resultatFin = resultat.concat(resultat1);
+      res.end(JSON.stringify(resultatFin));
+    }
+    else{
+      res.end(JSON.stringify(resultat));
+    }*/
   }
-
   else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
+
+
 });
+
+function convertirNumeroADiaTexto(numeroDia) {
+  const diasSemana = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  return diasSemana[numeroDia]; 
+}
+
+function convertirDiaTextoANumero(diaTexto) {
+  const diasSemana = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const numeroDia = diasSemana.indexOf(diaTexto);
+  return numeroDia !== -1 ? numeroDia : null;
+}
 
 function manejarConstraints(url) {
   const parsedUrl = new URL(url);
   const constraints = parsedUrl.searchParams;
   const constraintsSeparadas = {};
-
   for (const [key, value] of constraints.entries()) {
 
-    const [cleanKey, operator] = key.split(/\[(gt|lt|gte)\]/).filter(Boolean);
+      var [cleanKey, operator] = key.split(/\[(gt|lt|gte|lte)\]/).filter(Boolean);
 
-    if (!constraintsSeparadas[cleanKey]) {
-      constraintsSeparadas[cleanKey] = {};
-    }
-    if (operator) {
-
-      constraintsSeparadas[cleanKey][operator] = value;
-    } else {
-
-      constraintsSeparadas[cleanKey] = value;
-    }
+      if (!constraintsSeparadas[cleanKey]) {
+          constraintsSeparadas[cleanKey] = {};
+      }
+      if (operator) {
+          var casos = { gte: ">=", lte: "<=", gt: ">", lt: "<" };
+          operator = operator.replace(/gte|lte|lt|gt/gi, function (matched) {
+              return casos[matched];
+          });
+          constraintsSeparadas[cleanKey][operator] = value;
+      } else {
+          constraintsSeparadas[cleanKey] = value;
+      }
   }
   return constraintsSeparadas;
 }
